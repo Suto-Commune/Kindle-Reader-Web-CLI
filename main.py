ea29c287efc8853420ebcc355647b189d394ae79
hsn2 <912372447@qq.com>
2023-01-15 15:02:16 +0800

    优化代码 规范化变量 添加注释

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import requests as res
 from flask import render_template as temp
 import os
 
-app = fl(__name__, static_folder='', static_url_path='')
-
-url = "http://127.0.0.1:8080/reader3/"
-br = '<div class="wb"></div>'
-button = '<div id="cssbutton"><a id="btlogin" href="{link}">{str_}</a></div>'
+# 创建文件夹
 try:
-    os.system('mkdir storage\kindle\cover')
+    os.makedirs(os.path.join(os.getcwd(), 'storage', 'kindle', 'cover'))
 except:
     print(end='')
 
+# 创建flask对象
+app = fl(__name__, static_folder='', static_url_path='')
+
+# 初始化全局变量
+url = "http://127.0.0.1:8080/reader3/"
+br = '<div class="wb"></div>'
+button = '<div id="cssbutton"><a id="btlogin" href="{link}">{str_}</a></div>'
 style = ''' <style>.jz{text-align:center}.d-div{height:200;width:auto;text-align:center;display:flex;flex-direction
 :row;flex-wrap:wrap;justify-content:space-evenly;align-items:center}.wb{
 display:block;color:antiquewhite;height:20;width:auto}.xu-light{border:0;border-top:2px dotted#a2a9b6}#cssbutton{
@@ -26,28 +29,34 @@ dotted#a2a9b6}</style> '''
 
 @app.route("/")
 def index():
-    main_page = res.get(url + "getBookshelf")
-    main_page = main_page.json()
+    global url
+    main_page = res.get(url + "getBookshelf").json()
     return temp("bookshelf.html", main_page=main_page)
 
 
 @app.route('/book/')
 def book():
-    furl = req.full_path
-    burl = furl.replace("/book/?url=", "")
-    del furl
-    # 获取bookurl成功
+    global url, br
+    # 拼接url
+    f_url = req.full_path
+    b_url = f_url.replace("/book/?url=", "")
+    del f_url  # 其实可以不用del,py有GC的,运行完会自动删
 
-    shelf = res.get(url + "getBookshelf")
-    shelf = shelf.json()
-    book_info = dict()
+    # 获取book shelf
+    shelf = res.get(url + "getBookshelf").json()
+
+    book_info = {}
     for i in shelf['data']:
-        if i["bookUrl"] == burl:
+        if i["bookUrl"] == b_url:
             book_info = i
 
-    return temp("bookinfo.html", br=br, cover=book_info["coverUrl"], name=book_info["name"], author=book_info["author"],
+    return temp("bookinfo.html",
+                br=br, cover=book_info["coverUrl"],
+                name=book_info["name"],
+                author=book_info["author"],
                 intro=str(book_info["intro"]),
-                lastread=book_info["durChapterTitle"], latestread=book_info["latestChapterTitle"])
+                lastread=book_info["durChapterTitle"],
+                latestread=book_info["latestChapterTitle"])
 
 
 if __name__ == "__main__":
