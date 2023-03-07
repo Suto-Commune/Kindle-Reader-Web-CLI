600e21a90415fe9e603433d038918ce1f7f4a957
LolingNatsumi <63963655+miangou@users.noreply.github.com>
2023-03-07 22:11:35 +0800

    编译一份aarch64来增速

---
+++ docker/arm64/whl/install_whl.sh
@@ -0,0 +1,5 @@
+pip install greenlet-2.0.2-cp311-cp311-musllinux_1_1_aarch64.whl
+pip install setuptools-67.5.1-py3-none-any.whl
+pip install zope.event-4.6-py2.py3-none-any.whl
+pip install gevent-22.10.2-cp311-cp311-linux_aarch64.whl
+pip install zope.interface-5.5.2-cp311-cp311-linux_aarch64.whl
